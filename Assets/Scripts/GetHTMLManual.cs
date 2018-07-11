e86f4a127608711bea420a04eb116e45a04dcde9
red031000 <rubenru09@aol.com>
2018-07-11 19:57:17 +0100

    start of rule seed compatability

---
+++ Assets/Scripts/GetHTMLManual.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Assets.Scripts.RuleGenerator
+{
+	public partial class RadiatorRuleGenerator
+	{
+
+	}
+}
---
+++ Assets/Scripts/RadiatorRuleGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Assets.Scripts.RuleGenerator
+{
+	public partial class RadiatorRuleGenerator : AbstractRuleGenerator
+	{
+		public static RadiatorRuleGenerator Instance
+		{
+			get { return (RadiatorRuleGenerator)GetInstance<RadiatorRuleGenerator>(); }
+		}
+
+		public override string GetModuleType()
+		{
+			return "radiator";
+		}
+
+		public override void CreateRules()
+		{
+			//create rules here
+		}
+
+		public override string GetHTMLManual(out string filename)
+		{
+			filename = "Radiator.html";
+			if (!Initialized)
+				throw new Exception("You must Initialize the Random number generator first.");
+			if (!RulesGenerated)
+				throw new Exception("You must generate the rules first");
+
+			return new RadiatorManual().Manual.Replace("RULEGENERATORSEED", Seed.ToString()); //add replaces here once RNG has been implemented
+		}
+
+		public override string[] GetTextFiles(out string[] textFilePaths)
+		{
+			textFilePaths = RadiatorManual.TextAssetPaths;
+			return RadiatorManual.TextAssets;
+		}
+	}
+}
